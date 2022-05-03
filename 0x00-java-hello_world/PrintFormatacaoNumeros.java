bbdcc663da79a5ef1205b79bf75effa02a59015a
Diego Lopes Mercado <diegolm@ciandt.com>
2022-05-03 14:38:49 -0300

    sout texto

---
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -0,0 +1,7 @@
+public class PrintFormatacaoNumeros {
+    public static void main(String[] args) {
+        float taxa = 0.2456f;
+        float valor = 7654.321f;
+        System.out.println("R$ 7,654.32" + "\n" + "Taxa: 0.25%");
+    }
+}
