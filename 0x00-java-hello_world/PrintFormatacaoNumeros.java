3ede40eb23eb94ec3cfd9d47212dceabf24815bc
Diego Lopes Mercado <diegolm@ciandt.com>
2022-05-03 15:40:09 -0300

    sout texto

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -2,6 +2,11 @@ public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        System.out.println("Valor: R$ 7,654.32" + "\n" + "Taxa: 0.25%");
+//        System.out.println("Valor: R$ 7,654.32" + "\n" + "Taxa: 0.25%");
+        System.out.printf("Valor: " + "%,.2f\n", valor);
+        System.out.printf("Taxa: " + "%.2f", taxa);
+        System.out.println('%');
+
     }
 }
+
