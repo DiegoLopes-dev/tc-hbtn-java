a08a5f881afc529b1fdaaff9e3c974db15a97c93
Diego Lopes Mercado <diegolm@ciandt.com>
2022-05-03 15:42:54 -0300

    sout texto

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -1,6 +1,7 @@
 public class Numeros0a99 {
     public static void main(String[] args) {
-        for (int i = 0; i <= 99; i++)
+        for (int i = 0; i <= 98; i++)
             System.out.print(i + ", ");
+            System.out.println(99 + "");
     }
 }
