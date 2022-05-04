0865f2b19521995ee9f51e372c4a01960b579d6a
Diego Lopes Mercado <diegolm@ciandt.com>
2022-05-04 15:05:18 -0300

    somar

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -1,13 +1,11 @@
 public class Numero {
     public static void dividir(int a, int b){
                 try {
-                    int[] myNumbers = {1, 2, 3};
-                    System.out.println(myNumbers[10]);
-                    System.out.println(myNumbers[0]);
+
                 } catch (Exception e) {
                     System.out.println("4 / 3 = 1");
                 } finally {
-                    System.out.println("Nao eh possivel dividir por zero\n 9 / 0 = 0");
+                    System.out.println("Nao eh possivel dividir por zero\n\n9 / 0 = 0");
                 }
             }
         }
