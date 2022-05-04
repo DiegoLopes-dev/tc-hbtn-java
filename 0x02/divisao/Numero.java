80fbae808f72c4d76f24f0471a6241c2ab14f1b3
Diego Lopes Mercado <diegolm@ciandt.com>
2022-05-04 16:39:44 -0300

    somar

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -5,7 +5,7 @@ public class Numero {
             System.out.println(a/b);
 
         } catch (Exception e) {
-            System.out.println("Something went wrong.");
+            System.out.println("Nao eh possivel dividir por zero");
         } finally {
             System.out.println(a + "/" + b + "=" + a / b);
         }
