3aa1fc28e41c81948f12590e70653532e6d795e1
Diego Lopes Mercado <diegolm@ciandt.com>
2022-05-05 11:34:22 -0300

    lado

--- 0x02/quadrado/Quadrado.java
+++ 0x02/quadrado/Quadrado.java
@@ -1,11 +1,14 @@
 public class Quadrado {
     public static double area(double lado) throws Exception {
 
-        int result = 0;
+        double result = 0;
         try {
             if (lado < 0) {
             }
             throw new IllegalArgumentException(lado+ " é menor que 0.");
+            if (lado >= 1) {
+            }
+            result = lado * result;
         } catch (IllegalArgumentException e) {
             System.out.println("Lado deve possuir valor positivo");
         }
