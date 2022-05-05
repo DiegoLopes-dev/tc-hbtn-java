9dcf699bf3d9956cedc64f9b1081882c7f56be7e
Diego Lopes Mercado <diegolm@ciandt.com>
2022-05-05 17:16:56 -0300

    Salario

--- 0x03/salario/Gerente.java
+++ 0x03/salario/Gerente.java
@@ -2,7 +2,7 @@ public class Gerente extends Empregado{
     public Gerente(double salarioFixo) {
         super(salarioFixo);
     }
-
+@Override
     double calcularBonus(Departamento departamento) {
         if (departamento.alcancouMeta() == true) {
             double bonus = 0.20 * salarioFixo;
