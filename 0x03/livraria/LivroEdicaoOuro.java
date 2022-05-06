16efe6d93a06dde908b4649b48313fc07bc2dbdf
Diego Lopes Mercado <diegolm@ciandt.com>
2022-05-06 14:38:52 -0300

    Livro

--- 0x03/livraria/LivroEdicaoOuro.java
+++ 0x03/livraria/LivroEdicaoOuro.java
@@ -1,5 +1,8 @@
 public class LivroEdicaoOuro extends Livro{
 
+    public LivroEdicaoOuro() {
+    }
+
     public double getPreco() {
         Livro.this.preco = Livro.this.preco + 0.10;
         return preço;
