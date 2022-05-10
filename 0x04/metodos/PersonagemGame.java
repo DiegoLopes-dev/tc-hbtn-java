a46bea851a0573830dc1a15c34f2ce4df69a00a7
Diego Lopes Mercado <diegolm@ciandt.com>
2022-05-10 15:43:11 -0300

    Classes e Objetos

---
+++ 0x04/metodos/PersonagemGame.java
@@ -0,0 +1,31 @@
+public class PersonagemGame {
+        private int saudeAtual;
+        private String nome;
+
+        public int getSaudeAtual() {
+            return saudeAtual;
+        }
+
+        public void setSaudeAtual(int saudeAtual) {
+            this.saudeAtual = saudeAtual;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+    public void tomarDano(int quantidadeDeDano){
+        if(saudeAtual >= quantidadeDeDano){
+            saudeAtual = saudeAtual - quantidadeDeDano;
+        }
+    }
+    public void receberCura(int quantidadeDeCura){
+        if(saudeAtual + quantidadeDeCura == 100){
+            saudeAtual = saudeAtual + quantidadeDeCura;
+        }
+    }
+}
