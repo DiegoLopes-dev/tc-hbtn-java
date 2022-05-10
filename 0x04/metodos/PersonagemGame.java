15ea0ae741da4ecde913503ff0cd1d9c09f50570
Diego Lopes Mercado <diegolm@ciandt.com>
2022-05-10 16:06:54 -0300

    Classes e Objetos

--- 0x04/metodos/PersonagemGame.java
+++ 0x04/metodos/PersonagemGame.java
@@ -29,7 +29,7 @@ public class PersonagemGame {
         if(saudeAtual + quantidadeDeCura == 100){
             saudeAtual = saudeAtual + quantidadeDeCura;
         }else {
-            saudeAtual = 0;
+            saudeAtual = 100;
         }
     }
 }
