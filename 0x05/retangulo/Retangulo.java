0fddfdf5ab7c1a078b0a02a909f70c40c489f116
Diego Lopes Mercado <diegolm@ciandt.com>
2022-05-11 11:12:56 -0300

    Heranca

--- 0x05/retangulo/Retangulo.java
+++ 0x05/retangulo/Retangulo.java
@@ -1,11 +1,9 @@
 public class Retangulo {
     private double largura;
     private double altura;
-
     public double getLargura() {
         return largura;
     }
-
     public void setLargura(double largura) throws Exception {
         if(largura < 0){
             throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
@@ -13,11 +11,9 @@ public class Retangulo {
             this.largura = largura;
         }
     }
-
     public double getAltura(){
         return altura;
     }
-
     public void setAltura(double altura) throws Exception {
         if(altura < 0){
             throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
