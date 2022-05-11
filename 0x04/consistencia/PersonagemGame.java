a71ef761f5c691578fbc14a31d28f7eb83fe5a6a
Diego Lopes Mercado <diegolm@ciandt.com>
2022-05-11 09:03:12 -0300

    Classes e Objetos

---
+++ 0x04/consistencia/PersonagemGame.java
@@ -0,0 +1,56 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public PersonagemGame(int saudeAtual, String nome) {
+        this.setSaudeAtual(saudeAtual);
+        this.nome = nome;
+
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        if(saudeAtual > 0){
+            status = "vivo";
+        }else {
+            status = "morto";
+        }
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if(nome != null || nome != ""){
+            this.nome = nome;
+        }else{
+
+        }
+
+
+    }
+    public void tomarDano(int quantidadeDeDano){
+        if(saudeAtual - quantidadeDeDano >= 0){
+            this.setSaudeAtual(saudeAtual - quantidadeDeDano);
+        } else {
+            this.setSaudeAtual(0);
+        }
+    }
+    public void receberCura(int quantidadeDeCura){
+        if(saudeAtual + quantidadeDeCura <= 100){
+            this.setSaudeAtual(saudeAtual + quantidadeDeCura);
+        }else {
+            this.setSaudeAtual(100);
+        }
+    }
+}
