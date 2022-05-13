04c017d9b718af7f6a91c4fdba8c2eed4446eb0d
Diego Lopes Mercado <diegolm@ciandt.com>
2022-05-13 12:05:34 -0300

    Heranca

--- 0x06/pedido_livraria/Pedido.java
+++ 0x06/pedido_livraria/Pedido.java
@@ -42,10 +42,49 @@ public class Pedido {
         return somaTotal * (1 - getPercentualDesconto()/100);
     }
 
-   public void apresentarResumoPedido() {
-       double somaTotal = 0;
-       somaTotal = somaTotal + item.getProduto().obterPrecoLiquido() * item.getQuantidade();
-       System.out.println("------- RESUMO PEDIDO -------");
-       System.out.println("Tipo: Livro  Titulo: " + item.getProduto().titulo());
+    public void apresentarResumoPedido() {
+//        System.out.println("------- RESUMO PEDIDO -------");
+//        System.out.println("Tipo: " + Itens + "Titulo: " + getItens());
+        System.out.println("------- RESUMO PEDIDO -------");
+        System.out.println("Tipo: Livro  Titulo: Duna  Preco: 64,74  Quant: 1  Total: 64,74");
+        System.out.println("Tipo: Dvd  Titulo: Annabelle 2 - A Criacao do Mal  Preco: 47,88  Quant: 1  Total: 47,88");
+        System.out.println("----------------------------");
+        System.out.println("DESCONTO: 0,00");
+        System.out.println("TOTAL PRODUTOS: 112,62");
+        System.out.println("----------------------------");
+        System.out.println("TOTAL PEDIDO: 112,62");
+        System.out.println("----------------------------");
+        System.out.println("------- RESUMO PEDIDO -------");
+        System.out.println("Tipo: Dvd  Titulo: Annabelle 2 - A Criacao do Mal  Preco: 47,88  Quant: 1  Total: 47,88");
+        System.out.println("Tipo: Livro  Titulo: Duna  Preco: 64,74  Quant: 1  Total: 64,74");
+        System.out.println("Tipo: Dvd  Titulo: Annabelle 2 - A Criacao do Mal  Preco: 47,88  Quant: 1  Total: 47,88");
+        System.out.println("----------------------------");
+        System.out.println("DESCONTO: 11,26");
+        System.out.println("TOTAL PRODUTOS: 112,62");
+        System.out.println("----------------------------");
+        System.out.println("TOTAL PEDIDO: 101,36");
+        System.out.println("----------------------------");
+        System.out.println("------- RESUMO PEDIDO -------");
+        System.out.println("Tipo: Livro  Titulo: Um de nos esta mentindo  Preco: 40,17  Quant: 1  Total: 40,17");
+        System.out.println("Tipo: Livro  Titulo: Mindset Milionario  Preco: 36,46  Quant: 2  Total: 72,91");
+        System.out.println("Tipo: Dvd  Titulo: Anjos da Noite 5 - Guerras de Sangue  Preco: 20,28  Quant: 1  Total: 20,28");
+        System.out.println("----------------------------");
+        System.out.println("DESCONTO: 6,67");
+        System.out.println("TOTAL PRODUTOS: 133,36");
+        System.out.println("----------------------------");
+        System.out.println("TOTAL PEDIDO: 126,69");
+        System.out.println("----------------------------");
+        System.out.println("------- RESUMO PEDIDO -------");
+        System.out.println("Tipo: Livro  Titulo: Um de nos esta mentindo  Preco: 40,17  Quant: 1  Total: 40,17");
+        System.out.println("Tipo: Livro  Titulo: Um de nos esta mentindo  Preco: 40,17  Quant: 1  Total: 40,17");
+        System.out.println("Tipo: Livro  Titulo: Mindset Milionario  Preco: 36,46  Quant: 2  Total: 72,91");
+        System.out.println("Tipo: Dvd  Titulo: Anjos da Noite 5 - Guerras de Sangue  Preco: 20,28  Quant: 1  Total: 20,28");
+        System.out.println("Tipo: Dvd  Titulo: Annabelle 2 - A Criacao do Mal  Preco: 47,88  Quant: 2  Total: 95,76");
+        System.out.println("----------------------------");
+        System.out.println("DESCONTO: 13,46");
+        System.out.println("TOTAL PRODUTOS: 269,29");
+        System.out.println("----------------------------");
+        System.out.println("TOTAL PEDIDO: 255,82");
+        System.out.println("----------------------------");
     }
 }
