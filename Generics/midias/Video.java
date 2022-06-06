25e5670d2d713a4a7e3e249432131aca38c0d0b4
Diego Lopes Mercado <diegolm@ciandt.com>
2022-06-06 13:42:16 -0300

    midias

---
+++ Generics/midias/Video.java
@@ -0,0 +1,21 @@
+public class Video extends Midia {
+
+    private int duracao;
+
+    public Video() {
+
+    }
+
+    public Video(String nome, int duracao) {
+        setNome(nome);
+        this.duracao = duracao;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+}
