db8502c1721fc94e8723fa1d76f0165110c88d4c
Diego Lopes Mercado <diegolm@ciandt.com>
2022-06-08 14:39:29 -0300

    consultaProduto

--- Lambda_exercicios/consulta_produtos/ConsultaProdutos.java
+++ Lambda_exercicios/consulta_produtos/ConsultaProdutos.java
@@ -3,7 +3,7 @@ import java.util.stream.Collectors;
 
 public class ConsultaProdutos {
     
-    public static List<Produto> filtar (List<Produto> produtos, CriterioFiltro CriterioFiltro){
+    public static List<Produto> filtrar (List<Produto> produtos, CriterioFiltro CriterioFiltro){
         return produtos.stream().filter(p -> CriterioFiltro.testar(p)).collect(Collectors.toList());
     }
 }
