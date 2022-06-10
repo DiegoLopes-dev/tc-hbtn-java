8c2164132923a97e3df35eabd2cf2c2822335593
Diego Lopes Mercado <diegolm@ciandt.com>
2022-06-10 15:27:37 -0300

    pedidos eletronico desconto

--- Streams_Collectors_Optional/desconto_eletronicos/Consulta.java
+++ Streams_Collectors_Optional/desconto_eletronicos/Consulta.java
@@ -7,25 +7,20 @@ import java.util.stream.Collectors;
 public class Consulta {
 
     public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
-
         List<Produto> produtosPedidos = new ArrayList<Produto>(pedido.getProdutos());
-
-        List<Produto> filter = produtosPedidos.stream().filter(p -> p.getCategoria().equals(CategoriaProduto.LIVRO))
+        List<Produto> filter = produtosPedidos.stream()
+                .filter(p -> p.getCategoria()
+                        .equals(CategoriaProduto.LIVRO))
                 .collect(Collectors.toList());
-
         return filter;
-
     }
 
-
     public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
-
         return produtos.stream()
                 .max(Comparator.comparing(Produto::getPreco)).get();
     }
 
     public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, int precoMinimo) {
-
         List<Produto> produtoMenorPreco = produtos.stream()
                 .filter(p -> p.getPreco() >= precoMinimo)
                 .collect(Collectors.toList());
@@ -33,16 +28,25 @@ public class Consulta {
         return produtoMenorPreco;
     }
 
-
     public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
         return pedidos.stream()
                 .filter(pd -> pd.getProdutos().stream()
                         .anyMatch(p -> p.getCategoria().equals(CategoriaProduto.ELETRONICO)))
                 .collect(Collectors.toList());
-
     }
 
+    public static List<Produto>	aplicar15PorcentoDescontoEletronicos
+            (List<Produto> produtos) {
 
+        produtos.forEach(p ->{
+            if (p.getCategoria().equals(CategoriaProduto.ELETRONICO)) {
+                double desconto = p.getPreco()-(p.getPreco()*0.15);
+                p.setPreco(desconto);
+            }
+        });
+
+        return produtos;
+    }
 
 
 
