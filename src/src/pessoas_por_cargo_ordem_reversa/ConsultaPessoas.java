51b1bc6476247f619bca334ea9b016ac980d128a
Diego Lopes Mercado <diegolm@ciandt.com>
2022-06-13 15:41:59 -0300

    pessoas por cargo

--- src/src/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
+++ src/src/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
@@ -4,9 +4,10 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.groupingBy;
+
 public class ConsultaPessoas {
 
-    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+    static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas){
         return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, () -> new TreeMap<>(Comparator.reverseOrder()), Collectors.toCollection(TreeSet::new)));
     }
 }
