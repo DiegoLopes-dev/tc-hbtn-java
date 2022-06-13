fc20d561caf8b2e4b73c71b4f868bcc5f80c1168
Diego Lopes Mercado <diegolm@ciandt.com>
2022-06-13 15:32:08 -0300

    pessoas por cargo

--- src/src/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
+++ src/src/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
@@ -3,6 +3,7 @@ package pessoas_por_cargo_ordem_reversa;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.groupingBy;
 public class ConsultaPessoas {
 
     public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
